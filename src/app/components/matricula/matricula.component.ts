42b7b27bf8145f60e3a70fb2865a7f3421a41787
Felipe Junges <felipejunges@yahoo.com.br>
2022-09-02 18:28:04 -0300

    🚧 Criada primeira versão do POST da matrícula, incompleta

--- src/app/components/matricula/matricula.component.ts
+++ src/app/components/matricula/matricula.component.ts
@@ -13,18 +13,35 @@ export class MatriculaComponent implements OnInit {
 
   postagemForm!: FormGroup;
 
+  estados = [
+    { uf: 'RS', nome: 'Rio Grande do Sul' }
+  ];
+
   constructor(private service: MatriculaApiService) { }
 
   ngOnInit(): void {
     this.postagemForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      cpf: new FormControl('', [Validators.required, GenericValidators.isValidCpf()])
+      cpf: new FormControl('', [Validators.required, GenericValidators.isValidCpf()]),
+      identidade: new FormControl('', [Validators.required]),
+      dataNascimento: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)\d{2}$/)]),
+      telefone: new FormControl('', [Validators.required, Validators.pattern(/^\([0-9]{2}\)\s?[0-9]{5}(-?)[0-9]{4}$/)]),
+      logradouro: new FormControl('', [Validators.required]),
+      numero: new FormControl('', [Validators.required]),
+      complemento: new FormControl(''),
+      cidade: new FormControl('', [Validators.required]),
+      estado: new FormControl('', [Validators.required]),
+      cep: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]{5}(-)[0-9]{3}$/)])
     });
   }
 
   campoInvalido(campo: string) {
     return this.postagemForm.controls[campo].invalid && (this.postagemForm.controls[campo].dirty || this.postagemForm.controls[campo].touched);
+  }
+
+  obterValorCampo(campo: string): string {
+    return this.postagemForm.controls[campo].value;
   }
 
   // get nome() {
@@ -35,21 +52,21 @@ export class MatriculaComponent implements OnInit {
     if (this.postagemForm.invalid)
       return;
 
-    // this.postagemForm.setErrors({
-    //   cpf: 'teste'
-    // });
+    this.postagemForm.controls['cpf'].setErrors({ 'teste': true });
 
-    this.postagemForm.controls['cpf'].setErrors({'teste': true});
-
-    // this.service.incluir({
-    //   email: '',
-    //   senha: '',
-    //   nome: this.nome.value
-    // }).subscribe({
-    //   next: (v) => console.log(v),
-    //   error: (e) => console.error(e),
-    //   complete: () => console.info('complete')
-    // });
+    this.service.incluir({
+      email: this.obterValorCampo('email'),
+      senha: '',
+      nome: this.obterValorCampo('nome'),
+      cpf: this.obterValorCampo('cpf'),
+      identidade: this.obterValorCampo('identidade'),
+      dataNascimento: this.obterValorCampo('dataNascimento'),
+      telefone: this.obterValorCampo('telefone')
+    }).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
   }
 
 }
--- src/app/interfaces/Matricula.ts
+++ src/app/interfaces/Matricula.ts
@@ -1,5 +1,46 @@
 export interface Matricula {
+    nome: string,
     email: string,
     senha: string,
-    nome: string
+
+    cpf: string,
+    identidade: string,
+    dataNascimento: string,
+    telefone: string,
+
+    /*public string Nome { get; set; }
+
+        Email
+
+        Senha
+
+        public string Cpf { get; set; }
+
+        public string Identidade { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Telefone { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public int CursoId { get; set; }
+
+        public int NumeroParcelas { get; set; }
+
+        public DateTime DataPrimeiroVencimento { get; set; }*/
+
+        /*public string Cep { get; private set; }
+
+        public string Logradouro { get; private set; }
+
+        public string Numero { get; private set; }
+
+        public string? Complemento { get; private set; }
+
+        public string Bairro { get; private set; }
+
+        public string Municipio { get; private set; }
+
+        public string Uf { get; private set; } */
 }
