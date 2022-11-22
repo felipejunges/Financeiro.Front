b27489de8c1902dac4649990b832e521a0525b1d
Felipe Junges <felipejunges@yahoo.com.br>
2022-11-22 00:07:28 -0300

    🍻 Criado Dockerfile

--- src/app/init/keycloak-init.factory.ts
+++ src/app/init/keycloak-init.factory.ts
@@ -4,13 +4,11 @@ export function initializeKeycloak(keycloak: KeycloakService) {
     return () =>
         keycloak.init({
             config: {
-                url: 'http://localhost:8080',
+                url: 'http://keycloak:8080',
                 realm: 'myrealm',
                 clientId: 'myclient'
             },
             initOptions: {
-                //onLoad: 'check-sso',
-                //silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html'
                 silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
                 redirectUri: 'http://localhost:4200',
                 checkLoginIframe: false,
